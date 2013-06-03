a536b31ea89b2a6a8a32b695a41f5ac2e9ed87c5
gli <gli@redhat.com>
2013-06-03 14:42:24 +0800

    add README

---
+++ example/src/main/java/liangty/common/pagination/example/servlet/ShowSummaryServlet.java
@@ -0,0 +1,27 @@
+package liangty.common.pagination.example.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gli
+ * Date: 6/3/13
+ * Time: 2:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ShowSummaryServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
